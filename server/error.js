08bd9a72445a3c27180482753c8d003b7d43b451
Alvaro Cabero <alvarocabero@gmail.com>
2021-11-05 21:04:35 +0100

    :arrow_up: Added Server Error Management

---
+++ server/error.js
@@ -0,0 +1,18 @@
+const chalk = require("chalk");
+
+const debug = require("debug")("robots:errors");
+
+const notFoundErrorHandler = (req, res) => {
+  res.status(404).json({ error: "Endpoint not Found" });
+};
+
+const generalErrorHandler = (error, req, res, next) => {
+  debug(chalk.pink("An error occurred: ", error.message));
+  const message = error.code ? error.message : "General Error";
+  res.status(error.code || 500).json({ error: message });
+};
+
+module.exports = {
+  notFoundErrorHandler,
+  generalErrorHandler,
+};
